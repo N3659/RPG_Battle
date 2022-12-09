e29ca2b3c3d9080e2db031f4072659b7d284bea8
N3659 <76428379+N3659@users.noreply.github.com>
2022-12-09 13:04:59 +0000

    Add files via upload

---
+++ Code_baston.js
@@ -0,0 +1,55 @@
+//Heros
+
+Bob = document.getElementById("Bob")
+var DegatsBob = 20
+Boi = document.getElementById("Boi")
+var DegatsBoi = 10
+Destructor = document.getElementById("Destructor")
+var DegatsDestructor = 12
+RandomMan = document.getElementById("Random_Man")
+var DegatsRandomMan = 14
+
+//Mobs
+
+MrTongue = document.getElementById("Tongue")
+var DegatsTongue = 10
+C5 = document.getElementById("C5")
+var DegatsC5 = 17
+Bobby = document.getElementById("Bobby")
+var DegatsBobby = 18
+
+//Pv Heros
+
+PVBob = document.getElementById("PvBob")
+PVBoi = document.getElementById("PvBoi")
+PVDestructor = document.getElementById("PvDestructor")
+PVRandomMan = document.getElementById("PvRandom_Man")
+
+//Pv Mobs
+
+PvTongue = document.getElementById("PvTongue")
+PvC5 = document.getElementById("PvC5")
+PvBobby = document.getElementById("PvBobby")
+
+//Boutons
+
+Attaque = document.getElementById("Attaque")
+AttaqueSpe = document.getElementById("AttaqueSpe")
+Defense = document.getElementById("Defense")
+
+//Zone Affichage
+
+Affichage = document.getElementById("Affichage")
+
+tour = 0
+
+function joueurAttaque(joueur,ennemi,PvMobs)
+
+Attaque.onclick = function(){
+
+    Affichage.innerHTML = "Quel personnage attaque ?"
+    
+    console.log("bruh")
+
+}
+
